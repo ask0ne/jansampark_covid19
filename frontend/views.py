e5629330a69a8067c838422b0a76419b84f8ce60
kulkarniatharv <atharvpkulkarni1108@gmail.com>
2020-04-13 19:43:25 +0530

    Added location functionality
    Only retailers who have account registered with the same name as the organization will be able to update their items. Previously it was like any logged in user can add or update any organization's items which was not a good idea.
    Added location while adding new products

--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from .forms import RetailerForm
+from django.contrib.auth.models import User
+from retailers.models import Retailer 
 from django.contrib.auth.decorators import login_required
 
 
@@ -13,17 +15,25 @@ def get_data(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
-        form = RetailerForm(request.POST)
+        if User.objects.filter(username=request.POST["company_name"]).exists():
+            company_name = request.POST["company_name"]
+            product = request.POST["product"]
+            quantity = request.POST["quantity"]
+            lat = request.POST["lat"]
+            long= request.POST["long"]
+
+            record = Retailer(company_name=company_name, product=product, quantity=quantity, lat=lat, long=long)
+            record.save()
+            # form = RetailerForm(request.POST)
         # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # redirect to a new URL:
-            form.save()
+            # if form.is_valid():
+                # process the data in form.cleaned_data as required
+                # redirect to a new URL:
+                # form.save()
             return HttpResponseRedirect(reverse('frontend:index'))
 
     # if a GET (or any other method)
-    else:
-        form = RetailerForm()
+    form = RetailerForm()
 
 
     context = {'form': form}
